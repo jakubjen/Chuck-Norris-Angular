62588ea477fac770101c063a60f14e976ac6f015
Jakub Jen <poczta.jakub.jen@gmail.com>
2022-07-29 15:00:19 +0200

    Install ngx-translate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, SharedModule],
+  imports: [
+    BrowserModule,
+    SharedModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'en',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
+  ],
   providers: [],
+  exports: [TranslateModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
