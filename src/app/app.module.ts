e534ab959ba7ebdc0a815627a8e5f2103466a00e
Jakub Jen <poczta.jakub.jen@gmail.com>
2022-07-29 12:17:41 +0200

    Add head and input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [BrowserModule, SharedModule],
-  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ContainerComponent } from './container/container.component';
+import { InputComponent } from './input/input.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
-  exports: [],
+  declarations: [ContainerComponent, InputComponent],
+  imports: [CommonModule, FormsModule],
+  exports: [ContainerComponent, InputComponent],
 })
 export class SharedModule {}
