37e5256256a9bbab214a55f4a9876d6a643a3c84
Jakub Jen <poczta.jakub.jen@gmail.com>
2022-08-03 12:12:31 +0200

    Add custom select component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { InputComponent } from './input/input.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { CustomSelectComponent } from './custom-select/custom-select.component';
 
 @NgModule({
-  declarations: [InputComponent],
+  declarations: [InputComponent, CustomSelectComponent],
   imports: [CommonModule, FormsModule, TranslateModule],
-  exports: [InputComponent],
+  exports: [InputComponent, CustomSelectComponent],
 })
 export class SharedModule {}
