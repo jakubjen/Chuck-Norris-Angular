39821fccd0a50e5435e29c8517a2aceb6f461b9f
Jakub Jen <poczta.jakub.jen@gmail.com>
2022-08-08 10:04:54 +0200

    Add click outside directive

--- src/app/shared/counter/counter.component.spec.ts
+++ src/app/shared/counter/counter.component.spec.ts
@@ -8,9 +8,8 @@ describe('CounterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CounterComponent ]
-    })
-    .compileComponents();
+      declarations: [CounterComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CounterComponent);
     component = fixture.componentInstance;
